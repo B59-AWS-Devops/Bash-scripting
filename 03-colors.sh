450c2c4bca3390aa80da3c4ec5271f602fa97456
pavanraju13 <pavanraju4513@gmail.com>
2025-04-04 00:00:46 +0530

    02-print.sh file changes

---
+++ 03-colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Print the text in different colors
+
+#Colors                   foreground                   Background
+Red                        31                            41
+Green                      32                            42
+Yellow                     33                            43
+Blue                       34                            44
+Magentia                   35                            45
+Cyan                       36                            46
+
+
+#syntax to print colors
+
+echo -e "\e[31m I am printing Red color \e[0m"
+echo -e "\e[32m I am printing Green color \e[0m"
+echo -e "\e[33m I am printing yellow color \e[0m"
+echo -e "\e[34m I am printing Blue color \e[0m"
+echo -e "\e[35m I am printing Magentia color \e[0m"
+echo -e "\e[36m I am printing cyan color \e[0m"
