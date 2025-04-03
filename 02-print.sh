d5d6b59f8bda78343d3582e0bc56c24e6da1dfc5
pavanraju13 <pavanraju4513@gmail.com>
2025-04-03 23:31:37 +0530

    02-print.sh file changes

---
+++ 02-print.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo hai
+echo welcome
+echo to
+echo Devops
+
+# \n : move the cursor to the next line
+# \t : move the cursor to the next tab
+
+echo "hai \n welcome \n to \n Devops
