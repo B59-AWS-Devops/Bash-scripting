ec592de25e6ef806d6dd4ef0f8e3bf40403df6fa
pavanraju13 <pavanraju4513@gmail.com>
2025-04-04 11:58:04 +0530

    03-color.sh-changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,5 +6,8 @@
 #VARNAME=$DATA
 
 a=10              #declare variable a is 10
+b=20              #declare variable b is 20
 
-echo -e "\e[32m printa value is \e[33m${a} \e[0m"
+echo -e "\e[32m print a value is \e[33m${a} \e[0m"
+
+echo -e "\e[42; 31m print b value is  \e[35m${b} \e[0m"
