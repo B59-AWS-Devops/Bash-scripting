0ccf33e0623004911ed6e3788fded587145162c1
pavanraju13 <pavanraju4513@gmail.com>
2025-04-04 18:05:06 +0530

    03-color.sh-changes

--- 05-specialvariables.sh
+++ 05-specialvariables.sh
@@ -5,7 +5,9 @@
 a=500
 TEAM=$1
 echo "Team name is $TEAM"
+sleep 4
 echo -e "\e[32m print a value is $a \e[0m"
+sleep 4
 echo print no of arguments passed is $#
 echo print all the arguments passed is $@
 echo print all the arguments passed is $*
