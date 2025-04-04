95f06ce22b5163d740baed4cb471dc8bbf11e8c3
pavanraju13 <pavanraju4513@gmail.com>
2025-04-04 12:05:08 +0530

    03-color.sh-changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -12,3 +12,10 @@ echo -e "\e[32m print a value is \e[33m${a} \e[0m"
 
 
 echo -e "\e[42;31m print b value is  \e[35m${b} \e[0m"
+
+#"Welcome to Shell Scripting!" in Green
+
+#"Have a great day!" in Yellow
+
+echo -e "\e[32m "Welcome to shell scripting! \e[0m"
+echo -e "\e[33m "Have a great day! \e[0m"
