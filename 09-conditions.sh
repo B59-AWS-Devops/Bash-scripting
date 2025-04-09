bf4ade3985c869cf5b49419a80c43d1931bc8287
pavanraju13 <pavanraju4513@gmail.com>
2025-04-09 20:20:25 +0530

    auto push bash scripting

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -17,7 +17,7 @@ if [ "$ACTION" == "START" ]; then
 elif [ "$ACTION" == "STOP" ]; then
     echo -e "\e[32m"Stopping the service"\e[0m"
 elif [ "$ACTION" == "STATUS" ]; then
-    echo -e"\e[34m"status of the service"\e[0m"
+    echo -e "\e[34m"status of the service"\e[0m"
 else
     echo "Invalid argument!"
     echo -e "Usage:\n\t\e[36m$0 START|STOP|STATUS\e[0m"
